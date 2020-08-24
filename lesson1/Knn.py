66dda822e54dfa36e36162790a4e79221a43cfe6
Your Name <you@example.com>
2020-08-25 00:15:35 +0800

    提交作业

---
+++ lesson1/Knn.py
@@ -0,0 +1,21 @@
+from sklearn.model_selection import  train_test_split
+from  sklearn import preprocessing
+from sklearn.metrics   import accuracy_score
+from sklearn.datasets import load_digits
+from sklearn.neighbors  import KNeighborsClassifier
+
+digits = load_digits()
+data = digits.data
+train_x,test_x,train_y,test_y = train_test_split(data,digits.target,test_size=0.15,random_state=11)
+
+ss = preprocessing.StandardScaler()
+train_ss_x =  ss.fit_transform(train_x)
+test_ss_x = ss.fit_transform(test_x)
+
+crt = DecisionTreeRegressor()
+crt.fit(train_ss_x,train_y)
+predict_y = crt.predict(test_ss_x)
+print(accuracy_score(predict_y,test_y))
+
+
+# 数字识别用分类也可以用回归方法
